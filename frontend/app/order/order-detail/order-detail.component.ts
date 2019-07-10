d9313a8a22af0ddb1f978adfb8a534e56d1de73a
Charlie Kassel <charlie.kassel@vice.com>
2019-07-10 21:32:36 +0100

    feat: added frontend routing for detail component

--- frontend/app/order/order-detail/order-detail.component.ts
+++ frontend/app/order/order-detail/order-detail.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { OrderService } from '../order.service';
+import { Order } from '../models/interfaces';
 
 @Component({
   selector: 'app-order-detail',
   templateUrl: './order-detail.component.html',
-  styleUrls: ['./order-detail.component.scss']
+  styleUrls: ['./order-detail.component.scss'],
+  providers: [OrderService]
 })
 export class OrderDetailComponent implements OnInit {
 
-  constructor() { }
+  order: Order;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly orderService: OrderService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.orderService
+        .getOrder(params.id)
+        .subscribe(order => this.order = order);
+    });
   }
 
 }
--- frontend/app/order/order.service.ts
+++ frontend/app/order/order.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Order } from './models/interfaces';
-// import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class OrderService {
+  private readonly baseUrl = 'http://localhost:4300';
+
   constructor(private readonly http: HttpClient) {}
 
-  getOrders(page = 1) {
-    return this.http.get<Order[]>(`http://localhost:4300/orders?page=${page}`);
+  getOrders(page = 1): Observable<Order[]> {
+    return this.http.get<Order[]>(`${this.baseUrl}/orders?page=${page}`);
+  }
+
+  getOrder(id: number): Observable<Order> {
+    return this.http.get<Order>(`${this.baseUrl}/orders/${id}`);
   }
 }
